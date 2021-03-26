17e8347a0c0b7711f64cf818047f680006f9d7ae
Timothy Kanarsky <tkanarsky@outlook.com>
2021-03-26 00:27:12 -0700

    Create AlarmListener classes, integrate config

--- src/platform.ts
+++ src/platform.ts
@@ -1,18 +1,12 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
-
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import { AlarmListenerAccessory } from './platformAccessory';
+import { access } from 'node:fs';
 
-/**
- * HomebridgePlatform
- * This class is the main constructor for your plugin, this is where you should
- * parse the user config and discover/register accessories with Homebridge.
- */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class AlarmListenerPlatform implements DynamicPlatformPlugin {
+  
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
-
-  // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
 
   constructor(
@@ -20,16 +14,12 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
-    this.log.debug('Finished initializing platform:', this.config.name);
+    this.log.debug('Finished initializing alarm listener platform.');
 
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
     this.api.on('didFinishLaunching', () => {
       log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
-      this.discoverDevices();
+      // initialize alarm listener after accessories are uncached
+      this.initAlarmListener();
     });
   }
 
@@ -38,8 +28,6 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
    * It should be used to setup event handlers for characteristics and update respective values.
    */
   configureAccessory(accessory: PlatformAccessory) {
-    this.log.info('Loading accessory from cache:', accessory.displayName);
-
     // add the restored accessory to the accessories cache so we can track if it has already been registered
     this.accessories.push(accessory);
   }
@@ -49,68 +37,35 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
    * Accessories must only be registered once, previously created accessories
    * must not be registered again to prevent "duplicate UUID" errors.
    */
-  discoverDevices() {
+  initAlarmListener() {
 
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
+    const uuid = this.api.hap.uuid.generate('AlarmListener');
+    const listener = this.accessories.find(accessory => accessory.UUID === uuid);
 
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+    if (listener) {
 
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+      this.api.updatePlatformAccessories([listener]);
 
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+      // We've already initialized the alarm listener
+      this.log.info('Restoring existing alarm listener accessory');
 
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+      // create the accessory handler for the restored accessory
+      // this is imported from `platformAccessory.ts`
+      new AlarmListenerAccessory(this, listener);
 
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
+    } else {
+      // the accessory does not yet exist, so we need to create it
+      this.log.info('Creating new alarm listener accessory');
 
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
+      // create a new accessory
+      const accessory = new this.api.platformAccessory('Alarm Listener', this.api.hap.uuid.generate('AlarmListener'));
 
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+      // create the accessory handler for the newly create accessory
+      // this is imported from `platformAccessory.ts`
+      new AlarmListenerAccessory(this, accessory);
 
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
-
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-      }
+      // link the accessory to your platform
+      this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
     }
   }
 }
--- src/worker.ts
+++ src/worker.ts
@@ -1,7 +1,6 @@
 import numjs = require('numjs');
-import Analyzer = require('audio-analyser');
 import mic = require('mic');
-import { ExampleHomebridgePlatform } from './platform';
+import { AlarmListenerPlatform } from './platform';
 
 export class Detector {
 
@@ -9,18 +8,13 @@ export class Detector {
     sampling_rate: number;
 
     constructor(
-        private readonly platform: ExampleHomebridgePlatform,
-        private readonly device: string,
-        private readonly freq: number, 
-        private readonly tolerance: number,
-        private readonly decay: number,
-        private readonly thresh: number,
+        private readonly platform: AlarmListenerPlatform,
     ) {
         this.activation = NaN;
         this.sampling_rate = 16000;
         const microphone = mic({
             rate: this.sampling_rate.toString(),
-            device: this.device,
+            device: this.platform.config.mic_device,
             channels: '1',
             encoding: 'signed-integer',
             bitwidth: 16,
@@ -35,38 +29,34 @@ export class Detector {
     process(data: Buffer) {
         const data_array = numjs.array([...data]);
         const fft_array = numjs.stack([data_array, numjs.zeros(data_array.shape)], -1);
-        // console.log(fft_array);
-        // const fft_array = numjs.concatenate(data_array, numjs.zeros([data_array.shape, 1]))
         const bins_complex = numjs.fft(fft_array);
+
         const reals = bins_complex.slice(null, [0,1]).flatten();
-        // console.log(bins_complex);
-        // console.log(reals);
         const imags = bins_complex.slice(null, [1,2]).flatten();
-        // console.log(imags);
         const mirrored_bins = numjs.sqrt(numjs.add(numjs.multiply(reals, reals), numjs.multiply(imags, imags)));
-
         const bins = mirrored_bins.slice([Math.floor(mirrored_bins.shape[0] / 2)]);
-            // console.log(bins.shape[0]);
-        
-        const lower_idx = Math.round((bins.shape[0]) * (this.freq - this.tolerance) / this.sampling_rate);
-        const upper_idx = Math.round((bins.shape[0]) * (this.freq + this.tolerance) / this.sampling_rate);
-
+           
+        const lower_idx = Math.round((bins.shape[0]) * (this.platform.config.frequency - this.platform.config.tolerance) / this.sampling_rate);
+        const upper_idx = Math.round((bins.shape[0]) * (this.platform.config.frequency + this.platform.config.tolerance) / this.sampling_rate);
 
         const target_mean = numjs.mean(bins.slice([lower_idx, upper_idx]));
         const other_mean = numjs.mean(bins.slice([lower_idx])) + numjs.mean(bins.slice(upper_idx));
 
         const ratio = target_mean / other_mean;
 
-
         if (isNaN(this.activation)) this.activation = ratio;
         else {
-            const decay_constant = (this.sampling_rate / bins.shape[0]) * this.decay;
-            this.activation -= this.activation / decay_constant;
-            this.activation += ratio / decay_constant;
+            const inertia_constant = (this.sampling_rate / bins.shape[0]) * this.platform.config.inertia;
+            this.activation -= this.activation / inertia_constant;
+            this.activation += ratio / inertia_constant;
         }
     }
 
     activated() {
-        return this.activation >= this.thresh;
+        return this.activation >= this.platform.config.threshold;
+    }
+
+    getActivation() {
+        return this.activation;
     }
 }
